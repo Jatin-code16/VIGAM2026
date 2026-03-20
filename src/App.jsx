bb92742e4785535b7b45c6a7ff8d21218c6416e0
Jatin-code16 <jatinnaiknawa2@gmail.com>
2026-03-20 18:12:49 +0530

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-yellow-400">
+        🎬 VIGAM 2026 is alive!
+      </h1>
+    </div>
+  )
+}
+
+export default App
